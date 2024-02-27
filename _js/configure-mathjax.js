fe51156b662c8c2b0114a6b29b1889b2b0f816a9
陈宇轩 <1979865782@qq.com>
2024-02-27 21:45:36 +0800

    Updates

---
+++ _js/configure-mathjax.js
@@ -0,0 +1,13 @@
+window.MathJax = {
+    tex: {
+      inlineMath: [['$', '$'], ['\\(', '\\)']],
+      displayMath: [['$$', '$$'], ['\\[', '\\]']],
+      processEscapes: true,
+      processEnvironments: true
+    },
+    options: {
+      skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
+      ignoreHtmlClass: '.*|',
+      processHtmlClass: 'arithmatex'
+    }
+  };
